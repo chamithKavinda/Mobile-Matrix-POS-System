7a3a46aecbee62359d8d07cc9a36ac1f57c8dc27
chamithKavinda <chamith13kavinda@gmail.com>
2024-06-01 19:07:25 +0530

    implement Bind row to fields function

--- controller/CustomerController.js
+++ controller/CustomerController.js
@@ -12,50 +12,6 @@ function clearCustomerInputFields(){
     setBtnGroupCustomer();
 }
 
-// ------------ save btn event ------------
-$("#btnSaveCustomer").click(function(){
-    if(checkAllCustomer()){
-        saveCustomer();
-    }else{
-        alert("Something Went wrong! Please Check again.");
-    }
-});
-
-// ------------ save function ------------
-function saveCustomer(){
-    let customerId = $("$txtCusId").val();
-
-    //check customer if exixts or not
-    if(searchCustomer(customerId.trim()) == undefined){
-        let customerName = $("#txtCusName").val();
-        let customerAddress = $("#txtCusAddress").val();
-        let customerSalary = $("#txtCusSalary").val();
-
-        let newCustomer = object.assign({}, CustomerModel);
-
-        newCustomer.id = customerId;
-        newCustomer.name = customerName;
-        newCustomer.address = customerAddress;
-        newCustomer.salary = customerSalary;
-
-        customersDB.push(newCustomer);
-        clearCustomerInputFields();
-        getAllCustomer();
-        alert("Customer added sucessfully!!");
-        loadCustomerIds();
-    }else{
-        alert("Customer Exists!!.Please Try Again")
-        clearCustomerInputFields();
-    }
-}
-
-// ------------ search DB function ------------
-function searchCustomer(customerId){
-    return customersDB.find(function(customer){
-        return customer.id == customerId;
-    });
-}
-
 // ------------ Get all btn event ------------
 $("#getAll").click(function(){
     getAllCustomer();
@@ -85,3 +41,21 @@ function getAllCustomer(){
         bindTableRowEventsCustomer();
     }
 }
+
+// --------------Bind row to fields function---------------------------
+function bindTableRowEventsCustomer() {
+    $("#tblCustomer>tr").click(function (){
+        let id = $(this).children().eq(0).text();
+        let name = $(this).children().eq(1).text();
+        let address = $(this).children().eq(2).text();
+        let salary = $(this).children().eq(3).text();
+
+        $("#txtCusId").val(id);
+        $("#txtCusName").val(name);
+        $("#txtCusAddress").val(address);
+        $("#txtCusSalary").val(salary);
+
+        $("#btnDeleteCustomer").prop("disabled", false);
+    });
+}
+
