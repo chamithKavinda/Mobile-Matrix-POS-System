cc6a885e546b4b43a0348b385e24b223d63eca48
chamithKavinda <chamith13kavinda@gmail.com>
2024-06-01 18:20:08 +0530

    customer save option implemenetd

--- controller/CustomerController.js
+++ controller/CustomerController.js
@@ -3,7 +3,7 @@ $("#btnClearCustomer").click(function(){
     clearCustomerInputFields();
 });
 
-// ------------ clear btn function ------------
+// ------------ clear function ------------
 function clearCustomerInputFields(){
     $("#txtCusId, #txtCusName, #txtCusAddress, #txtCusSalary").val("");
     $("#txtCusId").focus();
@@ -11,3 +11,38 @@ function clearCustomerInputFields(){
 }
 
 // ------------ save btn event ------------
+$("#btnSaveCustomer").click(function(){
+    if(checkAllCustomer()){
+        saveCustomer();
+    }else{
+        alert("Something Went wrong! Please Check again.");
+    }
+});
+
+// ------------ save function ------------
+function saveCustomer(){
+    let customerId = $("$txtCusId").val();
+
+    //check customer if exixts or not
+    if(searchCustomer(customerId.trim()) == undefined){
+        let customerName = $("#txtCusName").val();
+        let customerAddress = $("#txtCusAddress").val();
+        let customerSalary = $("#txtCusSalary").val();
+
+        let newCustomer = object.assign({}, CustomerModel);
+
+        newCustomer.id = customerId;
+        newCustomer.name = customerName;
+        newCustomer.address = customerAddress;
+        newCustomer.salary = customerSalary;
+
+        customersDB.push(newCustomer);
+        clearCustomerInputFields();
+        getAllCustomer();
+        alert("Customer added sucessfully!!");
+        loadCustomerIds();
+    }else{
+        alert("Customer Exists!!.Please Try Again")
+        clearCustomerInputFields();
+    }
+}
