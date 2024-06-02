f6dce6871ac5055213564a24b2915b7a04380330
chamithKavinda <chamith13kavinda@gmail.com>
2024-06-02 08:42:59 +0530

    details added for itemsDB

--- db/DB.js
+++ db/DB.js
@@ -5,3 +5,11 @@ var customersDB = [
     {id:"C004",name:"Shehan dinusha",address:"Kandy",salaery:50000},
     {id:"C005",name:"Saduni kavindi",address:"Colombo",salaery:60000},
 ];
+
+var itemsDB = [
+    {code:"I001", name:"Air Pods", price: 6000.00, quantity: 50},
+    {code:"I002", name:"Charger", price: 2000.00, quantity: 20},
+    {code:"I003", name:"Back Cover", price: 1000.00, quantity: 20},
+    {code:"I004", name:"Iphone 13 pro max", price: 200000.00, quantity: 10},
+    {code:"I005", name:"Battery", price: 3000.00, quantity: 10},
+]
