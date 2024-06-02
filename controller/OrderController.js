fbf9cc4ff306b940f227d252289ac8b1644c837e
chamithKavinda <chamith13kavinda@gmail.com>
2024-06-02 21:32:18 +0530

    purchase btn event implemented

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -19,3 +19,16 @@ $("#btnPlaceOrder").on('click', function () {
     $("#tblPlaceOrder").empty();
     addToPlaceOrderTable();
 });
+
+$("#btnPurchase").on('click', function () {
+    if (parseFloat($("#cash").val())>= parseFloat($("#subTotal").text())){
+        placeOrderDetails();
+        clearPlaceOrderTextFields();
+        $("#tblPlaceOrder").empty();
+        cartDetails = [];
+        $("#orderId").val(generateNextOrderId());
+    }else {
+        alert("Insufficient credit! please check cash...");
+    }
+    loadAllOrderDetails();
+});
