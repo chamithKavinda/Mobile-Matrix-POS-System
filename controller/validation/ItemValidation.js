7b2b585be8a75eccbeb5581c8d8ef1c23a6670fa
chamithKavinda <chamith13kavinda@gmail.com>
2024-06-02 10:51:37 +0530

    add validations to the text fields

--- controller/validation/ItemValidation.js
+++ controller/validation/ItemValidation.js
@@ -1,5 +1,14 @@
-// ------regex for text fields-----
+// ------ regex for text fields -----
 const ITEM_CODE_REGEX = /^I\d{3}$/;
 const ITEM_NAME_REGEX = /^[A-Za-z ]{5,}$/;
 const ITEM_PRICE_REGEX =/^[0-9]{2,}([.][0-9]{2})?$/;
 const ITEM_QUANTITY_REGEX = /^\d+(\.\d+)?/;
+
+// ------- add validations to the text fields ----------
+let item_vArray = new Array();
+item_vArray.push({field: $("#txtItemCode"), regex: ITEM_CODE_REGEX});
+item_vArray.push({field: $("#txtItemName"), regex: ITEM_NAME_REGEX});
+item_vArray.push({field: $("#txtItemPrice"), regex: ITEM_PRICE_REGEX});
+item_vArray.push({field: $("#txtItemQuantity"), regex: ITEM_QUANTITY_REGEX});
+
+
