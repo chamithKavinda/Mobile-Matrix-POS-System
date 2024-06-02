1dfc0fae54962daf704e62d82c9d75249f6e3a7e
chamithKavinda <chamith13kavinda@gmail.com>
2024-06-02 09:07:21 +0530

    item section get all function implemented

--- controller/ItemController.js
+++ controller/ItemController.js
@@ -10,3 +10,33 @@ function clearItemInputFields(){
     $("#txtItemCode").focus();
     setBtnGroupItem();
 }
+
+// ------------ Get all btn event ------------
+$("#btnGetAllItem").click(function (){
+    getAllItem();
+})
+
+// --------------Get all  function---------------------------
+function getAllItem(){
+    $("#tblItem").empty();
+    $("#modalItemTable").empty();
+
+    for(let i=0; i<itemsDB.length; i++){
+        let id = itemsDB[i].code;
+        let name = itemsDB[i].name;
+        let price = itemsDB[i].price;
+        let quantity = itemsDB[i].quantity;
+
+        let row = `<tr>
+                        <td>${id}</td>
+                        <td>${name}</td>
+                        <td>${price}</td>
+                        <td>${quantity}</td>
+                   </tr>`;
+
+        $("#tblItem").append(row);
+        $("#modalItemTable").append(row);
+
+        bindTableRowEventsItem();
+    }
+}
