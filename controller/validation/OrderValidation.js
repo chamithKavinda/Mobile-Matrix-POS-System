e9aff32653c1de2c60b12f439b477ce515d3745d
chamithKavinda <chamith13kavinda@gmail.com>
2024-06-03 10:14:52 +0530

    clearPlaceOrderTextFields added

---
+++ controller/validation/OrderValidation.js
@@ -0,0 +1,12 @@
+const CUS_ID_ORDER_REGEX = /^C\d{3}$/;
+const ITEM_CODE_ORDER_REGEX = /^I\d{3}$/;
+
+let order_vArray = new Array();
+order_vArray.push({field: $("#cusId"), regex: CUS_ID_ORDER_REGEX});
+order_vArray.push({field: $("#itemId"), regex: ITEM_CODE_ORDER_REGEX});
+
+function clearPlaceOrderTextFields() {
+    $("#cusId,#cusName, #cusAddress,#cusSalary,#selectItemFormItem,#selectFormCustomer,#itemId,#itemName,#price,#quantity,#orderQty,#cash,#balance,#discount").val("");
+    $("#total").text(0.00);
+    $("#subTotal").text(0.00);
+}
