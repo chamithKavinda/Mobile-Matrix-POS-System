2e5c47d5f8ba09b1e2ede65da22ead58b3056f64
chamithKavinda <chamith13kavinda@gmail.com>
2024-06-04 11:19:45 +0530

    clearAll function implemented in dashboard

--- controller/DashboardController.js
+++ controller/DashboardController.js
@@ -1,5 +1,9 @@
 initUI();
 
+function clearAll(){
+    $("#homeContent , #customerContent , #itemContent , #orderContent").css("display","none");
+}
+
 function initUI(){
     clearAll();
     $("#homeContet").css("display","block");
