f14b74cd9eda87e0606b14de9c9028df75e7cb37
chamithKavinda <chamith13kavinda@gmail.com>
2024-06-04 11:03:12 +0530

    content update

--- db/DB.js
+++ db/DB.js
@@ -7,9 +7,19 @@ var customersDB = [
 ];
 
 var itemsDB = [
-    {code:"I001", name:"Air Pods", price: 6000.00, quantity: 50},
-    {code:"I002", name:"Charger", price: 2000.00, quantity: 20},
+    {code:"I001", name:"Air Pods", price: 6750.00, quantity: 50},
+    {code:"I002", name:"Charger", price: 2200.00, quantity: 20},
     {code:"I003", name:"Back Cover", price: 1000.00, quantity: 20},
     {code:"I004", name:"Iphone 13 pro max", price: 200000.00, quantity: 10},
     {code:"I005", name:"Battery", price: 3000.00, quantity: 10},
 ]
+
+var ordersDB = [
+    {oid:"OID-001", date:"2024/06/04", customerID:"C001",
+        orderDetails:[
+            {oid:"OID-001", code:"I001", qty:5, payment:6000.00},
+            {oid:"OID-001", code:"I002", qty:2, payment:2000.00}
+        ]
+    }
+];
+var cartDetails=[];
