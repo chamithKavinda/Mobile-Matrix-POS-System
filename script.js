8d4ba91b72f994928809a375eb4ed912b3c996c4
chamithKavinda <chamith13kavinda@gmail.com>
2024-07-23 10:50:59 +0530

    added hoverd class

---
+++ script.js
@@ -0,0 +1,9 @@
+// add hovered class in selected list item
+let list = document.querySelectorAll('.navigation li');
+function activeLink(){
+    list.forEach((item) =>
+    item.classList.remove('hovered'));
+    this.classList.add('hovered');
+}
+list.forEach((item) => 
+item.addEventListener('mouseover',activeLink));
